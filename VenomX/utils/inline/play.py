c98123c502048362a799b9d79cf0ab4f5199e2f2
Ytghta <146053609+Ytghta@users.noreply.github.com>
2023-09-26 16:52:36 +0530

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -19,10 +19,10 @@ def track_markup(_, videoid, user_id, channel, fplay):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+                text="✯ ᴏᴡɴᴇʀ ✯", url="https://t.me/FUCK_OF_BOLTE",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/yuki_harsh",
+                text="✯ sᴜᴩᴩᴏʀᴛ ✯", url="https://t.me/yuki_harsh",
             )
         ],
         [
@@ -76,10 +76,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+                text="✯ ᴏᴡɴᴇʀ ✯", url="https://t.me/FUCK_OF_BOLTE",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/yuki_harsh",
+                text="✯ sᴜᴩᴩᴏʀᴛ ✯", url="https://t.me/yuki_harsh",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -98,10 +98,10 @@ def stream_markup(_, chat_id):
             ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url="https://t.me/Venom_bolti_public",
+                text="✯ ᴏᴡɴᴇʀ ✯", url="https://t.me/FUCK_OF_BOLTE",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="https://t.me/yuki_harsh",
+                text="✯ sᴜᴩᴩᴏʀᴛ ✯", url="https://t.me/yuki_harsh",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
