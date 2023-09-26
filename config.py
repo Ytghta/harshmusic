efdbcf8fff8a6fc17d26a175ba7abd7421116ffe
Ytghta <146053609+Ytghta@users.noreply.github.com>
2023-09-26 18:40:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/venommusic",
+    "https://github.com/Ytghta/New",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
